f7983e2203dfed7ff3820f6ec4d1911abbd34da1
aleksei.kazulin <Kazulin.job@gmail.com>
2022-06-23 03:49:06 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import { getVendor} from './vendor.js';
+import { getGallary } from './modules/gallary.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	getVendor();
+	getGallary();
+});
---
+++ src/js/modules/gallary.js
@@ -0,0 +1,5 @@
+export const getGallary = () => {
+    const carousel = new Carousel(document.querySelector(".js-carousel"), {
+        'slidesPerPage' : '1',
+    });
+}
